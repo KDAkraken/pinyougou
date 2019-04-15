7fb4f15acf62c17fa29fe60ae49ab3fb53f2c2f2
Kraken <291967278@qq.com>
2019-04-15 19:10:52 +0800

    第三天

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -1,15 +1,17 @@
 package com.pinyougou.manager.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.pinyougou.entity.PageResult;
-import com.pinyougou.entity.Result;
+
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import entity.PageResult;
+import entity.Result;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/brand")
@@ -70,5 +72,9 @@ public class BrandController {
     @RequestMapping("/search")
     public PageResult search(@RequestBody TbBrand brand, int page, int rows ){
         return brandService.findPage(brand, page, rows);
+    }
+    @RequestMapping("/selectOptionList")
+    public List<Map> selectOptionList(){
+        return brandService.selectOptionList();
     }
 }
--- pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -1,77 +1,113 @@
 package com.pinyougou.sellergoods.service.impl;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
-import com.pinyougou.entity.PageResult;
 import com.pinyougou.mapper.TbBrandMapper;
-import com.pinyougou.mapper.TbGoodsMapper;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.pojo.TbBrandExample;
+import com.pinyougou.pojo.TbBrandExample.Criteria;
 import com.pinyougou.sellergoods.service.BrandService;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
+import entity.PageResult;
 
+/**
+ * 服务实现层
+ * @author Administrator
+ *
+ */
 @Service
 public class BrandServiceImpl implements BrandService {
-    @Autowired
-    private TbBrandMapper tbBrandMapper;
 
-    @Override
-    public List<TbBrand> findAll() {
-        return tbBrandMapper.selectByExample(null);
-    }
+	@Autowired
+	private TbBrandMapper brandMapper;
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<TbBrand> findAll() {
+		return brandMapper.selectByExample(null);
+	}
 
-    @Override
-    public PageResult findPage(int pageNum, int pageSize) {
-        //利用PageHelper插件实现分页
-        //（MyBatis 分页插件）
-        PageHelper.startPage(pageNum, pageSize);
-        //封装到pageHelper的page对象中
-        Page<TbBrand> page =
-                (Page<TbBrand>) tbBrandMapper.selectByExample(null);
-        return new PageResult(page.getTotal(), page.getResult());
-    }
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<TbBrand> page=   (Page<TbBrand>) brandMapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
 
-    @Override
-    public void add(TbBrand brand) {
-        tbBrandMapper.insert(brand);
-    }
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(TbBrand brand) {
+		brandMapper.insert(brand);		
+	}
 
-    //根据id查询
-    @Override
-    public TbBrand findOne(Long id) {
-        return tbBrandMapper.selectByPrimaryKey(id);
-    }
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(TbBrand brand){
+		brandMapper.updateByPrimaryKey(brand);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public TbBrand findOne(Long id){
+		return brandMapper.selectByPrimaryKey(id);
+	}
 
-    @Override
-    public void update(TbBrand brand) {
-        tbBrandMapper.updateByPrimaryKey(brand);
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id:ids){
+			brandMapper.deleteByPrimaryKey(id);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(TbBrand brand, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		TbBrandExample example=new TbBrandExample();
+		Criteria criteria = example.createCriteria();
+		
+		if(brand!=null){			
+						if(brand.getName()!=null && brand.getName().length()>0){
+				criteria.andNameLike("%"+brand.getName()+"%");
+			}
+			if(brand.getFirstChar()!=null && brand.getFirstChar().length()>0){
+				criteria.andFirstCharLike("%"+brand.getFirstChar()+"%");
+			}
+	
+		}
+		
+		Page<TbBrand> page= (Page<TbBrand>)brandMapper.selectByExample(example);		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
 
-    }
+	@Override
+	public List<Map> selectOptionList() {
+		// TODO Auto-generated method stub
+		return brandMapper.selectOptionList();
+	}
 
-    @Override
-    public void delete(Long[] ids) {
-        for (Long id : ids) {
-            tbBrandMapper.deleteByPrimaryKey(id);
-        }
 
-    }
-
-    @Override
-    public PageResult findPage(TbBrand brand, int pageNum, int pageSize) {
-        PageHelper.startPage(pageNum, pageSize);
-        TbBrandExample example = new TbBrandExample();
-        TbBrandExample.Criteria criteria = example.createCriteria();
-        if (brand != null) {
-            if (brand.getName() != null && brand.getName().length() > 0) {
-                criteria.andNameLike("%"+brand.getName()+"%");
-            }
-            if(brand.getFirstChar()!=null && brand.getFirstChar().length()>0){
-                criteria.andFirstCharEqualTo(brand.getFirstChar());
-            }
-        }Page<TbBrand> page= (Page<TbBrand>)tbBrandMapper.selectByExample(example);
-        return new PageResult(page.getTotal(), page.getResult());
-    }
+	
 }
